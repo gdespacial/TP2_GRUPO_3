4160baf035ccfce16f5810f635194caa63b64e72
Ramiro <ramiroadal626@gmail.com>
2025-04-07 14:22:04 -0300

    agrego tabla

--- TP2_GRUPO_3/ejercicio1.aspx.cs
+++ TP2_GRUPO_3/ejercicio1.aspx.cs
@@ -19,16 +19,32 @@ namespace TP2_GRUPO_3
         {
            // Descomentar el código y continuar, para que compile bien. Gabriel.
             
-            /*
+            
             int cantidadUno = int.Parse(txtCantUno.Text);  // Castear los números ingresados en los textBoxs de cantidades.
             int cantidadDos = int.Parse(txtCantDos.Text);
             int total = cantidadUno + cantidadDos; // Esta linita de código me va a servir para que sume y no concatene las cant.
             string productoUno = txtProdUno.Text.ToString();  // Guardar los productos ingresados en variables String.
             string productoDos = txtProdDos.Text.ToString();
-            */
+
+            String tabla = "<table border = '1'>";
+            tabla += "<tr><th>Producto</th><th>Cantidad</th></tr>";
+            tabla += "<tr>";
+            tabla += "<td>" + productoUno + "</td>";
+            tabla += "<td>" + cantidadUno + "</td>";
+            tabla += "</tr>";
+            tabla += "<tr>";
+            tabla += "<td>" + productoDos + "</td>";
+            tabla += "<td>" + cantidadDos + "</td>";
+            tabla += "</tr>";
+            tabla += "<tr>";
+            tabla += "<td>TOTAL</td>";
+            tabla += "<td>" + total + "</td>";
+            tabla += "</tr>";
+
+            tabla += "</Table";
+            lblTabla.Text = tabla;
 
 
-            /// Faltaria crear la tabla, etc. Borrar este comentario!
         }
     }
 }
--- TP2_GRUPO_3/ejercicio1.aspx.designer.cs
+++ TP2_GRUPO_3/ejercicio1.aspx.designer.cs
@@ -69,6 +69,15 @@ namespace TP2_GRUPO_3
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvProdUno;
 
         /// <summary>
+        /// Control lblTabla.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTabla;
+
+        /// <summary>
         /// Control btnTabla.
         /// </summary>
         /// <remarks>
