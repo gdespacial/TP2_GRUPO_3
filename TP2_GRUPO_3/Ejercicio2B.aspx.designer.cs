246b5ab0e79586874ea3affdc8ec0edbdca77729
Ramiro <ramiroadal626@gmail.com>
2025-04-10 12:32:52 -0300

    transfiero value de ddlCiudad a lblZona en ejercicio2B

--- TP2_GRUPO_3/Ejercicio2B.aspx.designer.cs
+++ TP2_GRUPO_3/Ejercicio2B.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace TP2_GRUPO_3
         protected global::System.Web.UI.WebControls.Label lblApellido;
 
         /// <summary>
+        /// Control lblZona.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblZona;
+
+        /// <summary>
         /// Control lblTemas.
         /// </summary>
         /// <remarks>
